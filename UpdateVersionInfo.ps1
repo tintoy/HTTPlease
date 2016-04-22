ac934ec2c4ac224471a7f7ac5fc229fecf07e067
tintoy <tintoy@tintoy.io>
2016-04-22 12:41:06 +1000

    Sketch out script for updating version info during CI builds.

---
+++ UpdateVersionInfo.ps1
@@ -0,0 +1,39 @@
+Param(
+	[Parameter(Mandatory = $true)]
+	[int] $BuildNumber
+)
+
+$ErrorActionPreference = 'Stop'
+
+# AF - note that this doesn't currently update dependency versions (Powershell's JSON support sucks)
+
+Function Update-NetCoreProjectVersion([string] $ProjectFile)
+{
+	$project = Get-Content $ProjectFile | ConvertFrom-Json | ? { $_ -ne $null } | select -First 1
+    If (!$project) {
+        Write-Warning "Project file '$ProjectFile' does not contain valid JSON."
+
+		Return
+    }
+    If (!$project.version) {
+		Write-Warning "Project file '$ProjectFile' does not contain a valid project version."
+
+		Return
+	}
+
+	Write-Host "Updating project file '$ProjectFile'..."
+
+	$oldVersion = $project.version
+	$newVersion = $oldVersion + "-$BuildNumber"
+    Add-Member -InputObject $project -NotePropertyName 'version' -NotePropertyValue $newVersion -Force
+
+    $project | ConvertTo-Json | Set-Content -Path $ProjectFile
+    
+    Write-Host "Updated version for '$ProjectFile' from '$oldVersion' to '$newVersion'."
+}
+
+# Update version info for all project files.
+$projectFiles = Dir -File '.\src\HTTPlease.*\project.json', '.\test\HTTPlease.*\project.json'
+ForEach ($projectFile In $projectFiles) {
+	Update-NetCoreProjectVersion $projectFile
+}
