8717f8bb083e6f39eaff7f52e17847cad9cf15fd
tintoy <tintoy@tintoy.io>
2016-04-24 09:59:59 +1000

    Add first guess at how linux / osx build script might work.

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-dnu build src/HTTPlease.* test/HTTPlease.*.Tests --quiet
+# Defaults
+quietFlag="--quiet"
+
+# Command-line arguments
+while [[ $# > 0 ]]; do
+	key="$1"
+
+	case $key in
+		-?|--help
+			echo "Usage:\n\tbuild-all.sh [-v|--verbose]"
+		;;
+		
+		-v|--verbose)
+			quietFlag=""
+		;;
+
+		*)
+			echo "Invalid argument '${key}' (try using -? or --help)."
+		;;
+	esac
+done
+
+dnu build src/HTTPlease.* test/HTTPlease.*.Tests $quietFlag
