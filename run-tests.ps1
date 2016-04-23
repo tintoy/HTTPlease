4ce6bd37beadf51db0de9fd1ea06fdcc843b4913
tintoy <tintoy@tintoy.io>
2016-04-23 14:39:21 +1000

    Add x-platform scripts for running tests.

---
+++ run-tests.ps1
@@ -0,0 +1,8 @@
+$dnx = Get-Command dnx
+
+Function Invoke-DnxTests([string] $ProjectName) {
+	& $dnx -p ".\test\$ProjectName" test
+}
+
+Invoke-DnxTests -ProjectName HTTPlease.Core.Tests
+Invoke-DnxTests -ProjectName HTTPlease.Formaters.Tests
--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Running tests..."
-dnx -p ./test/HTTPlease.Core.Tests test
-dnx -p ./test/HTTPlease.Formatters.Tests test
+testProjects=`ls -d1 ./test/*.Tests`
+for testProject in $testProjects; do
+	echo "Running tests for project \"$testProject\"."
+	dnx -p "$testProject" test
+done
+
 echo "Done."
