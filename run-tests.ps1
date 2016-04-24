223917740399db64c2c62a3aa16ba70ffb742859
tintoy <tintoy@tintoy.io>
2016-04-24 16:31:09 +1000

    Add -Verbose switch for test script.

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,7 +1,18 @@
+Param(
+    [switch] $Verbose
+)
+
 $dnx = Get-Command dnx
 
+If ($Verbose) {
+    $verboseSwitch = '-verbose'
+}
+Else {
+    $verboseSwitch = ''
+}
+
 Function Invoke-DnxTests([string] $ProjectName) {
-	& $dnx -p ".\test\$ProjectName" test
+	& $dnx -p ".\test\$ProjectName" test $verboseSwitch
 }
 
 Invoke-DnxTests -ProjectName HTTPlease.Core.Tests
