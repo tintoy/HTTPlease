538c55018010ee064e90f0d30cce746649d4f43a
tintoy <tintoy@tintoy.io>
2016-04-26 08:57:19 +1000

    Add overloads to HttpRequest.Create.JsonFromUri(uri|string) that take a JsonSerializerSettings.

--- src/HTTPlease.Formatters.Json/FactoryExtensions.cs
+++ src/HTTPlease.Formatters.Json/FactoryExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace HTTPlease.Formatters.Json
 {
@@ -24,13 +25,36 @@ namespace HTTPlease.Formatters.Json
 			if (requestFactory == null)
 				throw new ArgumentNullException(nameof(requestFactory));
 
+			return requestFactory.JsonFromUri(requestUri, null);
+		}
+
+		/// <summary>
+		///		Create a new HTTP request that expects and uses JSON as its primary format.
+		/// </summary>
+		/// <param name="requestFactory">
+		///		The HTTP request factory.
+		/// </param>
+		/// <param name="requestUri">
+		///		The request URI (can be relative or absolute).
+		/// </param>
+		/// <param name="serializerSettings">
+		///		The JSON serialiser settings used by the <see cref="JsonFormatter"/>.
+		/// </param>
+		/// <returns>
+		///		The new <see cref="HttpRequest"/>.
+		/// </returns>
+		public static HttpRequest JsonFromUri(this HttpRequestFactory requestFactory, string requestUri, JsonSerializerSettings serializerSettings)
+		{
+			if (requestFactory == null)
+				throw new ArgumentNullException(nameof(requestFactory));
+
 			if (String.IsNullOrWhiteSpace(requestUri))
 				throw new ArgumentException("Argument cannot be null, empty, or composed entirely of whitespace: 'requestUri'.", nameof(requestUri));
 
 			return
 				requestFactory.FromUri(requestUri)
 					.ExpectJson()
-					.UseJson();
+					.UseJson(serializerSettings);
 		}
 
 		/// <summary>
@@ -50,13 +74,36 @@ namespace HTTPlease.Formatters.Json
 			if (requestFactory == null)
 				throw new ArgumentNullException(nameof(requestFactory));
 
+			return requestFactory.JsonFromUri(requestUri, null);
+		}
+
+		/// <summary>
+		///		Create a new HTTP request that expects and uses JSON as its primary format.
+		/// </summary>
+		/// <param name="requestFactory">
+		///		The HTTP request factory.
+		/// </param>
+		/// <param name="requestUri">
+		///		The request URI (can be relative or absolute).
+		/// </param>
+		/// <param name="serializerSettings">
+		///		The JSON serialiser settings used by the <see cref="JsonFormatter"/>.
+		/// </param>
+		/// <returns>
+		///		The new <see cref="HttpRequest"/>.
+		/// </returns>
+		public static HttpRequest JsonFromUri(this HttpRequestFactory requestFactory, Uri requestUri, JsonSerializerSettings serializerSettings)
+		{
+			if (requestFactory == null)
+				throw new ArgumentNullException(nameof(requestFactory));
+
 			if (requestUri == null)
 				throw new ArgumentNullException(nameof(requestUri));
 
 			return
 				requestFactory.FromUri(requestUri)
 					.ExpectJson()
-					.UseJson();
+					.UseJson(serializerSettings);
 		}
 	}
 }
