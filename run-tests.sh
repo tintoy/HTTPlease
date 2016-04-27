6c3a1bfccaa2aa865b74cb6f32caa33acc28ef65
tintoy <tintoy@tintoy.io>
2016-04-27 18:21:15 +1000

    Add Travis configuration config

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 testProjects=`ls -d1 ./test/*.Tests`
 for testProject in $testProjects; do
 	echo "Running tests for project \"$testProject\"."
-	dnx -p "$testProject" test
+	dnx -p "$testProject" test -verbose
 done
 
 echo "Done."
