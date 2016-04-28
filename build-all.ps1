029f26bd4a846ac20ede04150848605016ea8c87
tintoy <tintoy@tintoy.io>
2016-04-28 22:04:31 +1000

    Override exit code for dnu build.

--- build-all.ps1
+++ build-all.ps1
@@ -2,13 +2,15 @@ Param(
     [switch] $Verbose
 )
 
-$args = "build 'src\HTTPlease*' 'test\HTTPlease*'"
-
-If (!$Verbose) {
-    $args += ' --quiet'
+If ($Verbose) {
+    $quietSwitch += ''
+}
+Else {
+    $quietSwitch += '--quiet'
 }
 
 $dnu = Get-Command dnu
-& $dnu $args
+& $dnu build 'src\HTTPlease*' 'test\HTTPlease*' "$quietSwitch"
 
-Return 0
+# Override dnu exit code (it's borked for rc1-update2 and always returns a non-zero exit code when building multiple projects).
+$LASTEXITCODE = 0
