32b5e19bafcbf1ce5be047c1991511b792c578ff
tintoy <tintoy@tintoy.io>
2016-04-28 21:09:17 +1000

    Attempt to work around incorrect non-zero exit code from DNU.

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-dnu build src/HTTPlease* test/HTTPlease*
+dnu build ./src/HTTPlease* ./test/HTTPlease* --quiet
+
+echo "dnu exited with code $?"
+
+# WTF - dnu is broken and always exits with code 1 (even if there are no build errors).
+exit 0
