7117b9d70fb1ac08d0f8e7013d3431ca1bfd0905
tintoy <tintoy@tintoy.io>
2016-04-30 11:42:09 +1000

    Add basic abstractions for security.

---
+++ src/HTTPlease.Security/Abstractions/HttpRequestAuthenticationProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HTTPlease.Security.Abstractions
+{
+	using Core.Utilities;
+
+	/// <summary>
+	///		The base class for HTTP request authentication mechanisms.
+	/// </summary>
+	public abstract class HttpRequestAuthenticationProvider
+		: DisposableObject, IHttpRequestAuthenticationProvider
+	{
+		/// <summary>
+		///		Create new <see cref="HttpRequestAuthenticationProvider"/>.
+		/// </summary>
+		protected HttpRequestAuthenticationProvider()
+		{
+		}
+
+		/// <summary>
+		///		Asynchronously configure an outgoing request message to add information required for authentication.
+		/// </summary>
+		/// <param name="requestMessage">
+		///		The outgoing HTTP request message.
+		/// </param>
+		/// <param name="cancellationToken">
+		///		An optional <see cref="CancellationToken"/> that can be used to cancel the asynchronous operation.
+		/// </param>
+		/// <returns>
+		///		A <see cref="Task"/> representing the asynchronous operation.
+		/// </returns>
+		public abstract Task AddAuthenticationAsync(HttpRequestMessage requestMessage, CancellationToken cancellationToken = new CancellationToken());
+    }
+}
---
+++ src/HTTPlease.Security/Abstractions/IHttpRequestAuthenticationProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HTTPlease.Security.Abstractions
+{
+	/// <summary>
+	///		Represents a mechanism for adding authentication information to HTTP request messages.
+	/// </summary>
+	public interface IHttpRequestAuthenticationProvider
+	{
+		/// <summary>
+		///		Asynchronously configure an outgoing request message to add information required for authentication.
+		/// </summary>
+		/// <param name="requestMessage">
+		///		The outgoing HTTP request message.
+		/// </param>
+		/// <param name="cancellationToken">
+		///		An optional <see cref="CancellationToken"/> that can be used to cancel the asynchronous operation.
+		/// </param>
+		/// <returns>
+		///		A <see cref="Task"/> representing the asynchronous operation.
+		/// </returns>
+		Task AddAuthenticationAsync(HttpRequestMessage requestMessage, CancellationToken cancellationToken = default(CancellationToken));
+	}
+}
