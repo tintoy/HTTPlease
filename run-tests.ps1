43b5969a03fb151670156fcfa07a1d527435fc05
tintoy <tintoy@tintoy.io>
2016-05-29 09:50:55 +1000

    Fix broken test script, and switch to netstandard1.5 instead of netcoreapp1.0 as appropriate.

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,14 +1,3 @@
-Param(
-    [string] $BuildVersion
-)
-
-If ($BuildVersion) {
-    $env:DNX_BUILD_VERSION = $BuildVersion
-}
-Else {
-    $env:DNX_BUILD_VERSION = 'dev'
-}
-
 $dotnet = Get-Command dotnet
 
 Function Invoke-DnxTests([string] $ProjectName) {
