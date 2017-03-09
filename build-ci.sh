19c2d11da6af5100305fd2abc225cb187c475a46
tintoy <tintoy@tintoy.io>
2017-03-10 08:06:16 +1100

    Fix broken CI build script.

--- build-ci.sh
+++ build-ci.sh
@@ -10,23 +10,34 @@ BUILD_ID=${TRAVIS_JOB_ID:=dev}
 BUILD_VERSION_SUFFIX="beta4-${BUILD_ID}"
 
 # Build outputs go here.
-artifactsDirectory="$PWD/artifacts""
-if [-d $artifactsDirectory]; then
+artifactsDirectory="$PWD/artifacts"
+if [ -d $artifactsDirectory ]; then
 	rm -rf $artifactsDirectory
 fi
 
-echo 'Build:'
+echo ''
+echo 'Restoring packages...'
+echo ''
 
 dotnet restore /p:VersionSuffix=${BUILD_VERSION_SUFFIX}
+
+echo ''
+echo 'Building...'
+echo ''
+
 dotnet build --version-suffix ${BUILD_VERSION_SUFFIX}
 
-echo 'Test:'
+echo ''
+echo 'Testing...'
+echo ''
 
-testProjects=$(find ./tests -name 'HTTPlease.*Tests.csproj')
+testProjects=$(find ./test -name 'HTTPlease.*Tests.csproj')
 for testProject in $testProjects; do
 	dotnet test $testProject
 done
 
-echo 'Pack:'
+echo ''
+echo 'Packing...'
+echo ''
 
 dotnet pack --version-suffix ${BUILD_VERSION_SUFFIX} --output $artifactsDirectory
