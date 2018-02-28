40375f2a1ede87cd36044306edba188ca4aacc00
Adam Friedman <tintoy@tintoy.io>
2018-03-01 10:50:51 +1100

    Fail CI build if any tests fail.

--- test-ci.ps1
+++ test-ci.ps1
@@ -1,6 +1,15 @@
 $testDir = Join-Path $PSScriptRoot 'test'
 $testProjects = Get-ChildItem $testDir -Recurse -File -Filter '*.Tests.csproj'
 
+$failingProjects = @()
 ForEach ($testProject In $testProjects) {
 	dotnet test --no-build $testProject.FullName
+
+	If ($LASTEXITCODE) {
+		$failingProjects += $testProject.Name.Replace('.csproj', '')
+	}
+}
+
+If ($failingProjects) {
+	Throw "The following projects have one or more failing tests: [$([string]::Join(', ', $failingProjects))]."
 }
